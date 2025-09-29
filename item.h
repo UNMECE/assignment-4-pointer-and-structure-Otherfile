bf3727fc7f5d1d6b9b33cc46b16af35d2c58fcd0
Otherfile <sunny.suren122@gmail.com>
2025-09-29 13:17:46 -0600

    Added Implementations

--- item.h
+++ item.h
@@ -13,7 +13,7 @@ struct _Item
 typedef struct _Item Item;
 
 
-void add_item(Item *item_list, double price, char *sku, char *category, char *name, int index)
+void add_item(Item *item_list, double price, char *sku, char *category, char *name, int index);
 void free_items(Item *item_list, int size);
 double average_price(Item *item_list, int size);
 void print_items(Item *item_list, int size);
